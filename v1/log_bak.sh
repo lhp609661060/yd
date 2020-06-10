01dd2726ae2e147f2d8841d4d814f940707802f9
lhp609661060 <lhp609661060@gmail.com>
2020-06-10 16:03:06 +0800

    设计思路

--- v1/log_bak.sh
+++ v1/log_bak.sh
@@ -8,7 +8,7 @@ zip_file="$ip.$(date +%Y%m%d).zip *.log.*"
 echo $(date +%F%n%T) "开始压缩日志:" $zip_file
 zip $ip.$(date +%Y%m%d).zip *.log.*
 echo $(date +%F%n%T) "开始备份到远程服务器"
-sshpass -p "Sgcrm.RootZaq!2wsx" scp $ip.$(date +%Y%m%d).zip root@10.1.239.127:/root/service-log/
+sshpass -p "Sgcrm.RootZaq!2wsx" scp -P 10022 $ip.$(date +%Y%m%d).zip root@10.1.239.127:/root/service-log/
 echo $(date +%F%n%T) "开始移除当前目录下已备份的文件"
 rm -rf $ip.$(date +%Y%m%d).zip
 rm -rf *.log.*
